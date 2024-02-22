3a9b2b9f6128da72859a985bcf922d0ecbbd0ac5
Pavan-2929 <kapadiyapavan3218@gmail.com>
2024-02-22 16:41:01 +0530

    complete semester2 page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header'
 import Material from './pages/Material'
 import About from './pages/About'
 import Semester1 from './pages/semester/Semester1'
+import Semester2 from './pages/semester/Semester2'
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       <Route path='/' element={<Material/>}/>
       <Route path='/about' element={<About/>} />
       <Route path='/semester-1' element={<Semester1/>} />
+      <Route path='/semester-2' element={<Semester2/>} />
     </Routes>
     </BrowserRouter>
     </>
